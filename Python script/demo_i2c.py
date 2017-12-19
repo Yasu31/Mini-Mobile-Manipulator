57e16f3c6a4dab2502e7ff994f9ffa0934e66b15
Yasu <yasu313nori@gmail.com>
2017-12-19 17:18:25 +0900

    edit python

--- Python script/demo_i2c.py
+++ Python script/demo_i2c.py
@@ -16,11 +16,11 @@ bus = smbus.SMBus(1)
 address = 0x04
 
 #Slave Address 2
-address_2 = 0x05
+# address_2 = 0x05
 
 def writeNumber(value):
     bus.write_byte(address, value)
-    bus.write_byte(address_2, value)
+    # bus.write_byte(address_2, value)
     # bus.write_byte_data(address, 0, value)
     return -1
 
@@ -30,14 +30,17 @@ def readNumber():
     return number
 
 while True:
-	#Receives the data from the User
+	# Receives the data from the User
     data = raw_input("Enter the data to be sent : ")
+
+    # list() converts iterable to list
     data_list = list(data)
     for i in data_list:
     	#Sends to the Slaves
         writeNumber(int(ord(i)))
         time.sleep(.1)
 
+    # write out newline character
     writeNumber(int(0x0A))
 
 #End of the Script
