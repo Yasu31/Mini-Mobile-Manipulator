b8987b50ce061b5d2070c89aada8b5f145b60cf6
Yasu31-raspi <yasu313nori@gmail.com>
2018-01-02 11:23:41 +0900

    fix i2c to work with smbus-cffi

--- catkin_ws/src/mmm_control/scripts/arduino_ROS_bridge.py
+++ catkin_ws/src/mmm_control/scripts/arduino_ROS_bridge.py
@@ -2,21 +2,26 @@
 
 import i2c
 import rospy
-from sensor_msgs import JointState
+from sensor_msgs.msg import JointState
 
 def publishSensors():
     pub=rospy.Publisher('joint_states', JointState, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('joint_state_publisher_a', anonymous=True)
     rate=rospy.Rate(10)
+    jointState=JointState()
     while not rospy.is_shutdown():
         sensorInfos=i2c.readData()
+        jointNames=[]
         for i in range(7):
-            jointName="link"+str(i)+"_link"+str(i+1)+"_joint"
-            jointState=JointState()
-            jointState.name=jointName
-            jointState.position=sensorInfos[i]/100
-            pub.publish(jointState)
-            rate.sleep()
+            jointNames.append("link"+str(i)+"_link"+str(i+1)+"_joint")
+        jointState.header.stamp=rospy.Time.now()
+        jointState.name=jointNames
+        # angles are in degrees*100. Convert to radians
+        jointState.position=[i/100*3.1415/180 for i in sensorInfos]
+        jointState.velocity=[]
+        jointState.effort=[]
+        pub.publish(jointState)
+        rate.sleep()
 if __name__ == '__main__':
     try:
         publishSensors()
--- catkin_ws/src/mmm_control/scripts/i2c.py
+++ catkin_ws/src/mmm_control/scripts/i2c.py
@@ -6,23 +6,27 @@
 #GPIO3 -> SCL
 
 #Import the Library Requreid
-from smbus2 import SMBusWrapper, i2c_msg
+#from smbus2 import SMBusWrapper, i2c_msg
+from smbus import SMBus
 import time
 import struct
 
 # number of bytes we receive from Arduino
-NUM_BYTES=32
+NUM_BYTES=14
 
 # This is the address we setup in the Arduino Program
 #Slave Address 1
 address = 0x04
 
+bus=SMBus(1)
+
 # https://github.com/kplindegaard/smbus2
 # send NOUN and VERB
 def writeData(noun, verb):
     bytesToSend=struct.pack('!hh', noun, verb)
-    with SMBusWrapper(1) as bus:
-        bus.write_i2c_block_data(address, 0, bytesToSend)
+    global bus
+    if True:
+        bus.write_i2c_block_data(address, 0, list(bytesToSend))
     return -1
 
 def bytes2Int(bytes):
@@ -32,9 +36,10 @@ def bytes2Int(bytes):
 # reads bytes from Arduino and return it as list of integers
 def readData():
     intList=[]
-    with SMBusWrapper(1) as bus:
-        block=bus.read_i2c_block_data(address, 0, NUM_BYTES)
-        print(block)
+    global bus
+    if True:
+        block=bus.read_i2c_block_data(address, 0)#, NUM_BYTES)
+        #print(block)
         # block[0:1] make up a two-byte integer, with twos complement.
         # Next, we try to convert block[] to a list of integers.
         for i in range(int(NUM_BYTES/2)):
