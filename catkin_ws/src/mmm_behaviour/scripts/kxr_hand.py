23e1ea7de051b61751fa9a6fbf9e64b0252328ba
Yasu31 <yasu313nori@gmail.com>
2018-01-29 22:37:12 +0900

    add kxr_hand.py

---
+++ catkin_ws/src/mmm_behaviour/scripts/kxr_hand.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://wiki.ros.org/tf/Tutorials/Adding%20a%20frame%20%28Python%29
+import rospy
+import tf
+# requires YAML, rospkg, catkin_pkg
+
+if __name__=='__main__':
+    rospy.init_node('broadcaster_marker_to_hand', anonymous=True)
+    br=tf.TransformBroadcaster()
+    rate=rospy.Rate(10)
+    while not rospy.is_shutdown():
+        br.sendTransform((0.0, 0.0, 0.05),(0,0,0,1),rospy.Time.now(),"ar_marker_0","kxr_hand")
+        rate.sleep()
